47a824e204abe4fe0af69d919482df35fcd79042
Viktoriia Z <viktoria.dubinina@gmail.com>
2022-08-12 00:08:44 -0700

    add ability to add engenners into machines information

--- Factory/Controllers.cs/MachinesController.cs
+++ Factory/Controllers.cs/MachinesController.cs
@@ -51,20 +51,46 @@ namespace Factory.Controllers
             return View(thisMachine);
         }
 
-        //         public ActionResult Edit(int id)
-        //         {
-        //             var thisCuisine = _db.Cuisines.FirstOrDefault(cuisine => cuisine.CuisineId == id);
-        //             ViewBag.RestaurantId = new SelectList(_db.Restaurants, "RestaurantId", "Name");
-        //             return View(thisCuisine);
-        //         }
+        public ActionResult Edit(int id)
+        {
+            var thisMachine = _db.Machines.FirstOrDefault(machine => machine.MachineId == id);
+            ViewBag.EngineerId = new SelectList(_db.Engineers, "EngineerId", "Name");
+            return View(thisMachine);
+        }
 
-        //         [HttpPost]
-        //         public ActionResult Edit(Cuisine cuisine)
-        //         {
-        //             _db.Entry(cuisine).State = EntityState.Modified;
-        //             _db.SaveChanges();
-        //             return RedirectToAction("Index");
-        //         }
+        [HttpPost]
+        public ActionResult Edit(Machine machine, int EngineerId)
+        {
+            if (EngineerId != 0)
+            {
+                _db.EngineerMachine.Add(
+                    new EngineerMachine() { EngineerId = EngineerId, MachineId = machine.MachineId }
+                );
+            }
+            _db.Entry(machine).State = EntityState.Modified;
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult AddEngineer(int id)
+        {
+            var thisMachine = _db.Machines.FirstOrDefault(machine => machine.MachineId == id);
+            ViewBag.EngineerId = new SelectList(_db.Engineers, "EngineerId", "Name");
+            return View(thisMachine);
+        }
+
+        [HttpPost]
+        public ActionResult AddEngineer(Machine machine, int EngineerId)
+        {
+            if (EngineerId != 0)
+            {
+                _db.EngineerMachine.Add(
+                    new EngineerMachine() { EngineerId = EngineerId, MachineId = machine.MachineId }
+                );
+                _db.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
 
         //         public ActionResult Delete(int id)
         //         {
