10db98582d54a09260eba9c399d3d71ac0f90625
Viktoriia Z <viktoria.dubinina@gmail.com>
2022-08-12 15:21:29 -0700

    show routing in engineer controller and views

--- Factory/Controllers.cs/EngineersController.cs
+++ Factory/Controllers.cs/EngineersController.cs
@@ -1,81 +1,102 @@
-// using Microsoft.AspNetCore.Mvc.Rendering;
-// using Microsoft.EntityFrameworkCore;
-// using Microsoft.AspNetCore.Mvc;
-// using BestRestaurants.Models;
-// using System.Collections.Generic;
-// using System.Linq;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using Factory.Models;
+using System.Collections.Generic;
+using System.Linq;
 
-// namespace BestRestaurants.Controllers
-// {
-//     public class RestaurantsController : Controller
-//     {
-//         private readonly BestRestaurantsContext _db;
+namespace Factory.Controllers
+{
+    public class EngineersController : Controller
+    {
+        private readonly FactoryContext _db;
 
-//         public RestaurantsController(BestRestaurantsContext db)
-//         {
-//             _db = db;
-//         }
+        public EngineersController(FactoryContext db)
+        {
+            _db = db;
+        }
 
-//         public ActionResult Index()
-//         {
-//             List<Restaurant> model = _db.Restaurants.ToList();
-//             return View(model);
-//         }
+        public ActionResult Index()
+        {
+            return View(_db.Engineers.ToList());
+        }
 
-//         public ActionResult Create()
-//         {
-//             return View();
-//         }
+        public ActionResult Create()
+        {
+            ViewBag.MachineId = new SelectList(_db.Machines, "MachineId", "Type");
+            return View();
+        }
 
-//         [HttpPost]
-//         public ActionResult Create(Restaurant restaurant)
-//         {
-//             _db.Restaurants.Add(restaurant);
-//             _db.SaveChanges();
-//             return RedirectToAction("Index");
-//         }
+        [HttpPost]
+        public ActionResult Create(Engineer engineer, int MachineId)
+        {
+            _db.Engineers.Add(engineer);
+            _db.SaveChanges();
+            if (MachineId != 0)
+            {
+                _db.EngineerMachine.Add(
+                    new EngineerMachine()
+                    {
+                        MachineId = MachineId,
+                        EngineerId = engineer.EngineerId
+                    }
+                );
+                _db.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
 
-//         public ActionResult Details(int id)
-//         {
-//             Restaurant thisRestaurant = _db.Restaurants.FirstOrDefault(
-//                 restaurant => restaurant.RestaurantId == id
-//             );
-//             return View(thisRestaurant);
-//         }
+        public ActionResult Details(int id)
+        {
+            var thisEngineer = _db.Engineers
+                .Include(engineer => engineer.JoinEntities)
+                .ThenInclude(join => join.Machine)
+                .FirstOrDefault(engineer => engineer.EngineerId == id);
+            return View(thisEngineer);
+        }
 
-//         public ActionResult Edit(int id)
-//         {
-//             var thisRestaurant = _db.Restaurants.FirstOrDefault(
-//                 restaurant => restaurant.RestaurantId == id
-//             );
-//             return View(thisRestaurant);
-//         }
+        public ActionResult Edit(int id)
+        {
+            var thisEngineer = _db.Engineers.FirstOrDefault(engineer => engineer.EngineerId == id);
+            ViewBag.MachineId = new SelectList(_db.Machines, "MachineId", "Name");
+            return View(thisEngineer);
+        }
 
-//         [HttpPost]
-//         public ActionResult Edit(Restaurant restaurant)
-//         {
-//             _db.Entry(restaurant).State = EntityState.Modified;
-//             _db.SaveChanges();
-//             return RedirectToAction("Index");
-//         }
+        [HttpPost]
+        public ActionResult Edit(Engineer engineer, int MachineId)
+        {
+            if (MachineId != 0)
+            {
+                _db.EngineerMachine.Add(
+                    new EngineerMachine()
+                    {
+                        MachineId = MachineId,
+                        EngineerId = engineer.EngineerId
+                    }
+                );
+            }
+            _db.Entry(engineer).State = EntityState.Modified;
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
 
-//         public ActionResult Delete(int id)
-//         {
-//             var thisRestaurant = _db.Restaurants.FirstOrDefault(
-//                 restaurant => restaurant.RestaurantId == id
-//             );
-//             return View(thisRestaurant);
-//         }
+        // public ActionResult Delete(int id)
+        // {
+        //     var thisRestaurant = _db.Restaurants.FirstOrDefault(
+        //         restaurant => restaurant.RestaurantId == id
+        //     );
+        //     return View(thisRestaurant);
+        // }
 
-//         [HttpPost, ActionName("Delete")]
-//         public ActionResult DeleteConfirmed(int id)
-//         {
-//             var thisRestaurant = _db.Restaurants.FirstOrDefault(
-//                 restaurant => restaurant.RestaurantId == id
-//             );
-//             _db.Restaurants.Remove(thisRestaurant);
-//             _db.SaveChanges();
-//             return RedirectToAction("Index");
-//         }
-//     }
-// }
+        // [HttpPost, ActionName("Delete")]
+        // public ActionResult DeleteConfirmed(int id)
+        // {
+        //     var thisRestaurant = _db.Restaurants.FirstOrDefault(
+        //         restaurant => restaurant.RestaurantId == id
+        //     );
+        //     _db.Restaurants.Remove(thisRestaurant);
+        //     _db.SaveChanges();
+        //     return RedirectToAction("Index");
+        // }
+    }
+}
