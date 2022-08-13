14be22329c16098bbea4693d09c0e2827dc7e4ae
Viktoriia Z <viktoria.dubinina@gmail.com>
2022-08-12 22:50:51 -0700

    add functionality that can add machinnes to engineer details

--- Factory/Controllers.cs/EngineersController.cs
+++ Factory/Controllers.cs/EngineersController.cs
@@ -80,6 +80,30 @@ namespace Factory.Controllers
             return RedirectToAction("Index");
         }
 
+        public ActionResult AddMachine(int id)
+        {
+            var thisEngineer = _db.Engineers.FirstOrDefault(engineer => engineer.EngineerId == id);
+            ViewBag.MachineId = new SelectList(_db.Machines, "MachineId", "Type");
+            return View(thisEngineer);
+        }
+
+        [HttpPost]
+        public ActionResult AddMachine(Engineer engineer, int MachineId)
+        {
+            if (MachineId != 0)
+            {
+                _db.EngineerMachine.Add(
+                    new EngineerMachine()
+                    {
+                        MachineId = MachineId,
+                        EngineerId = engineer.EngineerId
+                    }
+                );
+                _db.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+
         public ActionResult Delete(int id)
         {
             var thisEngineer = _db.Engineers.FirstOrDefault(engineer => engineer.EngineerId == id);
